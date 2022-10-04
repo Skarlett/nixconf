1d2ba80aabb0b7808fa11314b6bed425539295bd
Lunarix <3759687+Skarlett@users.noreply.github.com>
2022-10-04 10:57:06 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
 
   outputs = { self, nixpkgs }: {
-    nixosConfigurations.container = nixpkgs.lib.nixosSystem {
+    nixosConfigurations = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules =
         [ ({ pkgs, ... }: {
