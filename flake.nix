63b9a437bd8e332b08ef77a500b381d45224040a
Lunarix <3759687+Skarlett@users.noreply.github.com>
2022-10-05 17:25:25 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,61 +1,22 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+  description = "Flake for rapid VPS";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.hardware.url = "github:nixos/nixos-hardware";
 
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules =
-        [ ({ pkgs, ... }: {
-            # boot.isContainer = true;
-            # Let 'nixos-version --json' know about the Git revision
-            # of this flake.
-            system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
-          
-            imports =
-              [ # Include the results of the hardware scan.
-                #./hardware-configuration.nix
-              ];
+  outputs = { nixpkgs, ... }@inputs: rec {
+    legacyPackages = nixpkgs.lib.genAttrs [ "x86_64-linux" "x86_64-darwin" ] (system:
+      import inputs.nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      }
+    );
 
-            # Bootloader.
-            boot.loader.grub.enable = true;
-            boot.loader.grub.device = "/dev/vda";
-  
-            networking.hostName = "nixos";
-  
-            # Set your time zone.
-            time.timeZone = "America/Chicago";
-
-            # Select internationalisation properties.
-            i18n.defaultLocale = "en_US.utf8";
-
-            # Define a user account. Don't forget to set a password with ‘passwd’.
-            users.users =
-              let 
-                  sshkey="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILcon6Pn5nLNXEuLH22ooNR97ve290d2tMNjpM8cTm2r lunarix@masterbook";
-              in 
-            {
-                lunarix = {
-                  isNormalUser = true;
-                  openssh.authorizedKeys.keys = [ sshkey ];
-                  description = "test";
-                  extraGroups = [ "wheel" ];
-                  initialPassword = "changeme";
-                  packages = with pkgs; [];
-                };
-                root.openssh.authorizedKeys.keys = [ sshkey ];
-            };
-  
-            environment.systemPackages = with pkgs; [];
-            services.openssh = {
-              enable = true;
-              passwordAuthentication = false;
-              permitRootLogin = "yes";
-            };
-  
-            system.stateVersion = "22.05";
-          })
-      ];
+    nixosConfigurations = {
+      vps = nixpkgs.lib.nixosSystem {
+        pkgs = legacyPackages.x86_64-linux;
+        specialArgs = { inherit inputs; }; # Pass flake inputs to our config
+        modules = [ ./configuration.nix ];
+      };
     };
-
   };
 }
